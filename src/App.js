a8e44cc3d5d6089e693c5ced06a6ce083ff497b9
Sergey Timechko <sergio.tim.1982.22@gmail.com>
2019-08-21 00:38:12 +0300

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button, DropdownButton, Dropdown } from 'react-bootstrap'
 
 function App() {
   return (
@@ -10,14 +11,14 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button variant="outline-danger">My Кнопка</Button>
+
+
+        <DropdownButton id="dropdown-basic-button" title="Dropdown button">
+          <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+          <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+          <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+        </DropdownButton>
       </header>
     </div>
   );
